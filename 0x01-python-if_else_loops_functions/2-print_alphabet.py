ca75a8e6d8258d1684c6b87c2fbdc5f98f69349f
Garret Akpale <garretakpale@gmail.com>
2023-09-05 10:38:07 -0700

    Print Alphanbets

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# ASCII value for lowercase 'a'
-start = ord('a')
-# ASCII value for lowercase 'z'
-end = ord('z')
-# Loop through the ASCII values and print the corresponding character
-for ascii_value in range(start, end + 1):
-    print(chr(ascii_value), end='')
+
+"""Print the alphabet in lowercase, not followed by a new line."""
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end='')
+
