b2f59799a6b85e02b243643b4fc2643ea8685279
Garret Akpale <garretakpale@gmail.com>
2023-09-13 12:54:32 -0700

    search and replace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
+# Computes square value of all integers of a matrix.
 
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
-# computes squares of all matrix
+
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
